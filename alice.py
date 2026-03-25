4a033a7dcfcd477ee647189e40a3f5159453a783
FarellaKamala046 <18223046@std.stei.itb.ac.id>
2026-03-26 00:30:15 +0700

    implement hashing, digital signature, and RSA key encryption

--- alice.py
+++ alice.py
@@ -1,9 +1,15 @@
 from Crypto.Cipher import AES
+from Crypto.Hash import SHA256
+from Crypto.Signature import pkcs1_15
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
 import os
+import base64
 
 plaintext = "Bob, cek situasi di lokasi X".encode()
 
-key = os.urandom(32)  # AES-256
+#ciphertext AES-256-CBC
+key = os.urandom(32)  #AES-256
 iv = os.urandom(16)
 
 pad_len = 16 - len(plaintext) % 16
@@ -12,9 +18,36 @@ plaintext_padded = plaintext + bytes([pad_len]) * pad_len
 cipher = AES.new(key, AES.MODE_CBC, iv)
 ciphertext = cipher.encrypt(plaintext_padded)
 
-import base64
 
 print("Plaintext:", plaintext.decode())
 print("Ciphertext:", base64.b64encode(ciphertext).decode())
 print("IV:", base64.b64encode(iv).decode())
 print("Key:", base64.b64encode(key).decode())
+
+#hash
+hash_obj = SHA256.new(plaintext)
+hash_bytes = hash_obj.digest()
+
+hash_hex = hash_bytes.hex()
+
+print("Hash:", hash_hex)
+
+#signature
+with open("keys/alice_private.pem", "rb") as f:
+    alice_private_key = RSA.import_key(f.read())
+    
+signature = pkcs1_15.new(alice_private_key).sign(hash_obj)  
+signature_b64 = base64.b64encode(signature).decode()
+
+print("Signature:", signature_b64)
+
+#load public key bob
+with open("keys/bob_public.pem", "rb") as f:
+    bob_public_key = RSA.import_key(f.read())
+    
+rsa_cipher = PKCS1_OAEP.new(bob_public_key)
+encrypted_key = rsa_cipher.encrypt(key)
+encrypted_key_b64 = base64.b64encode(encrypted_key).decode()
+
+print("Encrypted Key:", encrypted_key_b64)
+
